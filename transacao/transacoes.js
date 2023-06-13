18e9e24e5c8c79cd217885949cb64aea27731556
Adriel dos Santos <adrieldossantos7@gmail.com>
2023-06-12 21:07:20 -0300

    Adiciona edição de transação.

--- transacao/transacoes.js
+++ transacao/transacoes.js
@@ -1,4 +1,7 @@
-let currentConta;
+let currentConta; 
+
+let tipoOperacao = 0; // 0 = criar, 1 = editar
+let currentTransacaoId = 0; // identificação da transação a ser editada
 
 window.onload = () => {
     carregaConta();
@@ -51,7 +54,17 @@ function limpaValores() {
     setaValoresInput('#valor', null);
 }
 
-function validacaoValoresCriacaoTransacao(valoresInfos) {
+function atualizaSaldo(tipo, valor) {
+    if(tipo === 0) {
+        currentConta.saldo += valor;
+    } else {
+        currentConta.saldo -= valor;
+    }
+
+    addConteudoHTML('#saldoConta', formataValorReal(currentConta.saldo), false);
+}
+
+function validacaoValoresTransacao(valoresInfos) {
     return new Promise((resolve, reject) => {
         let contemInvalidos = false;
         const errorElement = '#criarTransacaoErrors';
@@ -78,48 +91,77 @@ function validacaoValoresCriacaoTransacao(valoresInfos) {
     })
 }
 
-function criaTransacao() {
+function operacaoTransacao() {
     const tipo = pegaValoresInput('#tipo');
     const categoria = pegaValoresInput('#categoria');
     const descricao = pegaValoresInput('#descricao');
     const valor = pegaValoresInput('#valor');
     const data = new Date().toLocaleDateString();
 
-    console.log(tipo, categoria, descricao, valor, data)
-
-    validacaoValoresCriacaoTransacao([
+    validacaoValoresTransacao([
         {valor: descricao, mensagem: 'Informe a Descrição da transação.'},
         {valor: categoria, mensagem: 'Informe a Categoria da transação.'},
         {valor: valor, mensagem: 'Informe o Valor da transação.'}
     ]).then(() => {
-            // Adicionando nova conta que foi validada
-            // const novaConta = new Conta(descricao, nomeBanco, agencia, numeroConta);
-            // currentContas.push(novaConta);
-            // // arruamr infos aqui
-            // addConteudoHTML(
-            //     '#tableBody', 
-            //     `<tr>
-            //         <th scope="row">${novaConta.id}</th>
-            //         <td>${novaConta.banco}</td>
-            //         <td>${novaConta.descricao}</td>
-            //         <td>${novaConta.agencia}</td>
-            //         <td>${novaConta.saldo}</td>
-            //         <td>
-            //             <button type="button" class="btn btn-primary" onclick="abrirConta('${novaConta.id}')">
-            //                 Infos
-            //             </button>
-            //             <button type="button" class="btn btn-warning" onclick="excluirConta('${novaConta.id}')">
-            //                 Mesclar
-            //             </button>
-            //             <button type="button" class="btn btn-danger" onclick="excluirConta('${novaConta.id}')">
-            //                 Excluir
-            //             </button>
-            //         </td>
-            //     </tr>`,
-            //     true
-            // );
+            const novaTransacao = new Transacao(descricao, data, Number(tipo), categoria, valor);
+
+            if(tipoOperacao === 0) {
+                // cria nova transação
+                currentConta.transacoes.push(novaTransacao);
+            } else {
+                // edita última transação
+                const currentTransacaoIndex = currentConta.transacoes.findIndex((item) => item.id === currentTransacaoId);
+                currentConta.transacoes[currentTransacaoIndex].descricao = novaTransacao.descricao;
+                currentConta.transacoes[currentTransacaoIndex].tipo = novaTransacao.tipo;
+                currentConta.transacoes[currentTransacaoIndex].valor = novaTransacao.valor;
+                currentConta.transacoes[currentTransacaoIndex].categoria = novaTransacao.categoria;
+            }
+            // atualizar na tabela ultima transação editada;
+            
+            addConteudoHTML(
+                '#tableBody', 
+                `<tr>
+                    <th scope="row">${novaTransacao.id}</th>
+                    <td>${novaTransacao.data}</td>
+                    <td>${transacaoTipoDescricao(novaTransacao.tipo)}</td>
+                    <td>${novaTransacao.categoria}</td>
+                    <td>${novaTransacao.descricao}</td>
+                    <td>${novaTransacao.valor}</td>
+                    <td>
+                        bla
+                    </td>
+                </tr>`,
+                true
+            );
+
+            atualizaSaldo(novaTransacao.tipo, novaTransacao.valor);
             limpaValores();
             fechaModal();
         }
     ).catch((err) => console.error('Ocorreu um erro ao criar a conta', err));
+}
+
+function abrirModalEditarUltimaTransacao() {
+    const indexUltimaTransacao = currentConta.transacoes.length - 1;
+
+    if(indexUltimaTransacao !== -1) {
+        const currentTransacao = currentConta.transacoes[indexUltimaTransacao];
+
+        currentTransacaoId = currentTransacao.id;
+        tipoOperacao = 1;
+
+        setaValoresInput('#tipo', currentTransacao.tipo);
+        setaValoresInput('#categoria', currentTransacao.categoria);
+        setaValoresInput('#descricao', currentTransacao.descricao);
+        setaValoresInput('#valor', currentTransacao.valor);
+
+        addConteudoHTML('#labelBtnPrimaryModal', 'Editar transação', false);
+        addConteudoHTML('#labelModalHeader', 'Edição de transação', false);
+    }
+}
+
+function abrirModalCriarTransacao() {
+    tipoOperacao = 0;
+    addConteudoHTML('#labelBtnPrimaryModal', 'Criar transação', false);
+    addConteudoHTML('#labelModalHeader', 'Criação de transação', false);
 }
